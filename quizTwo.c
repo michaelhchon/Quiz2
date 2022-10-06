0494b50ba3378bf42e2fce3cf7640af8b46be136
michaelhchon <mchon@cpp.edu>
2022-10-06 14:29:49 -0700

    Initial outline for tic tac toe game (no code)

---
+++ quizTwo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+//Tic Tac Toe game between 2 players or player vs computer
+
+int main() {
+    // prompt user for game they wish to play
+
+    //read information from console
+
+    //display back choice and current status of board (blank)
+
+    //if 1 (pvp)
+    //start game loop until win conidtion is met
+
+    //prompt p1 for move, receive input as row, column, then place 'X'
+
+    //display board
+
+    //prompt p2 for move receive input as row, column, then place 'O'
+    
+    //display board
+
+    //loop until EITHER 3 in a row or no more tiles to fill
+
+    //display either p1 wins or p2 wins
+
+    //if 2 (pvc)
+    //start game loop until win conidtion is met
+
+    //prompt p1 for move, receive input as row, column, then place 'X'
+
+    //display board
+
+    //generate random number for row and column, check if existing mark, then place 'O' if nothing
+
+    //display board
+
+    //loop until EITHER 3 in a row or no more tiles to fill
+
+    //display congrats or better luck next time, end program
+
+    return 0;
+}
